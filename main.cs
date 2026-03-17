519a4ee38d6960934d10035559b9f7239e55e1ed
faris <faris.rawls5@gmail.com>
2026-03-17 10:42:44 -0400

    hi

--- main.cs
+++ main.cs
@@ -4,18 +4,18 @@ namespace HelloWorld
 {
   class Program
   {
-        static void Main(string[] args)
-        {
+    static void Main(string[] args)
+    {
       if ( args.Length > 1)
-            {
-                for (int i = 0; i < args.Length; ++i)
-                {
-                    Console.WriteLine($"Argument {i}: {args[i]}");
-                }
-            } else
-            {
-                Console.WriteLine("No command line arguments were given.");
-            }
+      {
+      for (int i = 0; i < args.Length; ++i)
+      {
+          Console.WriteLine($"Argument {i}: {args[i]}");
+      }
+      } else
+      {
+          Console.WriteLine("No command line arguments were given.");
+      }
     }
   }
 }
